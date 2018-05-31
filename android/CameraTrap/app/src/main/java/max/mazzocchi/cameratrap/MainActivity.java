0cb39a4693d8ec6866b0afef954202cf2621db11
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-05-31 19:14:30 -0400

    Added listener for texture view, began permissions setup.

--- android/CameraTrap/app/src/main/java/max/mazzocchi/cameratrap/MainActivity.java
+++ android/CameraTrap/app/src/main/java/max/mazzocchi/cameratrap/MainActivity.java
@@ -1,5 +1,9 @@
 package max.mazzocchi.cameratrap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,8 +39,50 @@ public class MainActivity extends AppCompatActivity {
 
         if(preview_view.isAvailable()) {
             Log.i(TAG, "Available!");
+            openCamera(preview_view.getWidth(), preview_view.getHeight());
         } else {
-            Log.i(TAG, "Not available.");
+            preview_view.setSurfaceTextureListener(new PreviewTextureListener());
+        }
+    }
+
+    private void openCamera(int width, int height) {
+        if(permissionsGranted()) {
+            Log.i(TAG, "We have permissions.");
+        } else {
+            Log.i(TAG, "We don't have permissions.");
+        }
+    }
+
+    private boolean permissionsGranted() {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED);
+    }
+
+    private class PreviewTextureListener implements TextureView.SurfaceTextureListener {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+            Log.i(TAG, "Texture available");
+            Log.i(TAG, "Width: "+width);
+            Log.i(TAG, "Height: "+height);
+
+            openCamera(width, height);
+        }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
+            Log.i(TAG, "Texture changed");
+            Log.i(TAG, "Width: "+width);
+            Log.i(TAG, "Height: "+height);
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+            Log.i(TAG, "Texture destroyed");
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+            Log.i(TAG, "Texture updated");
         }
     }
 }
