49112fa5f7d194c6ed1abee626a8a29627373d4a
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-03 08:32:40 -0400

    Added some threading to handle images.

--- android/CameraTrap/app/src/main/java/max/mazzocchi/cameratrap/MainActivity.java
+++ android/CameraTrap/app/src/main/java/max/mazzocchi/cameratrap/MainActivity.java
@@ -3,22 +3,34 @@ package max.mazzocchi.cameratrap;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.TextureView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CameraTrap";
 
     private TextureView preview_view;
+    private HandlerThread image_handler_thread;
+    private Handler image_handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -27,8 +39,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preview_view = this.findViewById(R.id.camera_preview);
+        image_handler_thread = new HandlerThread("ImageHandler");
+        image_handler_thread.start();
+        image_handler = new Handler(image_handler_thread.getLooper());
 
         Log.i(TAG, "onCreate finished");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+
+        image_handler_thread.quitSafely();
+        try {
+            image_handler_thread.join();
+        } catch(InterruptedException ie) {
+            Log.e(TAG, "Image handler thread was interrupted while joining.", ie);
+        }
+
+        Log.i(TAG, "onDestroy complete");
     }
 
     @Override
@@ -43,8 +74,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onResume");
 
         if(preview_view.isAvailable()) {
-            Log.i(TAG, "Available!");
             openCamera(preview_view.getWidth(), preview_view.getHeight());
+
         } else {
             preview_view.setSurfaceTextureListener(
                     new PreviewTextureListener());
@@ -94,6 +125,17 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 CameraCharacteristics characteristics = getCameraCharacteristics(manager);
+                StreamConfigurationMap map = characteristics.get(
+                        CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                Size largest = Collections.max(
+                        Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                        new SizeComparator());
+                ImageReader reader = ImageReader.newInstance(
+                        largest.getWidth(), largest.getHeight(),
+                        ImageFormat.JPEG,
+                        2);
+                reader.setOnImageAvailableListener(new ImageAvailableListener(),
+                        image_handler);
 
             } catch(CannotAccessCameraException cace) {
                 Log.e(TAG, "Could not access camera: {0}", cace);
@@ -117,17 +159,13 @@ public class MainActivity extends AppCompatActivity {
         public void onSurfaceTextureAvailable(SurfaceTexture texture,
                                               int width, int height) {
             Log.i(TAG, "Texture available");
-            Log.i(TAG, "Width: "+width);
-            Log.i(TAG, "Height: "+height);
-
             openCamera(width, height);
         }
+
         @Override
         public void onSurfaceTextureSizeChanged(SurfaceTexture texture,
                                                 int width, int height) {
             Log.i(TAG, "Texture changed");
-            Log.i(TAG, "Width: "+width);
-            Log.i(TAG, "Height: "+height);
         }
 
         @Override
@@ -142,13 +180,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class CannotAccessCameraException extends Exception {
+    private static class CannotAccessCameraException extends Exception {
         public CannotAccessCameraException(Throwable e) {
             super(e);
         }
 
         public CannotAccessCameraException(String reason) {
             super(reason);
+        }
+    }
+
+    private static class SizeComparator implements Comparator<Size> {
+        @Override
+        public int compare(Size a, Size b) {
+            return Long.signum(
+                    (long) a.getWidth() * a.getHeight() -
+                    (long) b.getWidth() * b.getHeight());
+        }
+    }
+
+    private static class ImageAvailableListener
+            implements ImageReader.OnImageAvailableListener {
+
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = reader.acquireNextImage();
+            // TODO: Send the image off
         }
     }
 }
