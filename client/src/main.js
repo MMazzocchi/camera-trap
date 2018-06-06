dc20b17a404804c862da330a9417fcdc7b066515
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-06 18:38:52 -0400

    Added camera mirroring code.

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+async function setupCamera(vid) {
+  try {
+    var stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    vid.src = window.URL.createObjectURL(stream);
+    vid.play();
+
+  } catch(e) {
+    console.error("Could not open stream", e);
+  }
+};
+
+var video = document.getElementById("preview");
+
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  setupCamera(video);
+
+} else {
+  console.error("Media devices could not be accessed.");
+}
