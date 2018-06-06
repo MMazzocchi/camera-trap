c979320a9e5d993812f8053f1354bfc33fae970f
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-06 19:27:09 -0400

    Added a streaming flag variable to Video, added a button to the main page.

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,17 +1,31 @@
 Video = async function() {
   var that = {};
 
+  // Check for camera support
   if((!navigator.mediaDevices) || (!navigator.mediaDevices.getUserMedia)) {
     throw new Error("Camera devices unsupported.");
   }
 
   // Fields
+  var streaming = false;
   var stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
   // Public methods
   that.bind = function(video_el) {
     video_el.srcObject = stream;
     video_el.play();
+  };
+
+  that.start = function() {
+    streaming = true;
+  };
+
+  that.stop = function() {
+    streaming = false;
+  };
+
+  that.streaming = function() {
+    return streaming;
   };
 
   return that;
