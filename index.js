dccfe6667b2d331f3e2c3400a17e031642de48cb
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-06 17:54:43 -0400

    Added express and created client page.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world.");
+var join = require("path").join;
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+
+const PORT = 9221;
+const HOST = "0.0.0.0";
+
+app.use("/", express.static(join(__dirname, "client")));
+
+http.listen(PORT, HOST, function() {
+  console.log("Listening on "+HOST+": "+PORT);
+});
