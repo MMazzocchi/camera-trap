2b0c6e78c01a28c69c42a387d2442d5e83d55323
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-07 20:28:47 -0400

    Made button color change on click.

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,22 @@ var preview_el = document.getElementById("preview");
 Video()
 .then(function(video) {;
   video.bind(preview_el);
+
+  var streaming = false;
+
+  var button = document.getElementById("button");
+  var inner = document.getElementById("inner-button");
+  button.onclick = function(e) {
+    e.preventDefault();
+
+    if(streaming) {
+      inner.setAttribute("fill", "grey");
+    } else {
+      inner.setAttribute("fill", "red");
+    }
+
+    streaming = !streaming;
+  };
 })
 .catch(function(e) {
   console.error("Could not instantiate video", e);
