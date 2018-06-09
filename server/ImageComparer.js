6bce961ff618aae0bd6b2d1d698dac187de0cb2b
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-09 12:17:54 -0400

    Implemented a histogram comparison.

--- server/ImageComparer.js
+++ server/ImageComparer.js
@@ -1,3 +1,21 @@
+var cv = require("opencv4nodejs");
+
+const PARAMS = [
+  // HUE
+  {
+    "channel": 0,
+    "ranges": [0, 180],
+    "bins": 50
+  },
+
+  // SATURATION
+  {
+    "channel": 1,
+    "ranges": [0, 256],
+    "bins": 60
+  }
+];
+
 var start_id = 0;
 function newId() {
   var new_id = start_id;
@@ -11,17 +29,27 @@ var ImageComparer = function() {
   // Fields
   var id = newId();
   var debug = require("debug")("camera-trap:comparer:"+id);
-  var base_img = undefined;
+  var base_hist = undefined;
 
   // Public methods
-  that.handle = function(new_img) {
-    debug("Handling an image.");
+  that.handle = function(image_data) {
+    debug("Processing an image.");
 
-    if(base_img === undefined) {
-      base_img = new_img;
+    var buffer = Buffer.from(image_data, "base64");
+    var mat = cv.imdecode(buffer, cv.IMREAD_COLOR);
+    var hsv = mat.cvtColor(cv.COLOR_BGR2HSV);
+    var hist = cv.calcHist(hsv, PARAMS).convertTo(cv.CV_32F);
+    hist.normalize(0, 1, cv.NORM_MINMAX);
+
+    if(base_hist === undefined) {
+      base_hist = hist;
+
+    } else {
+      var metric = base_hist.compareHist(hist, 0);
+      console.log(metric);
     }
 
-    // TODO: Magic
+    debug("Completed image processing.");
   };
 
   return that;
