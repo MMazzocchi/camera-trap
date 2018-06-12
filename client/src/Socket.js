ad9b261d0e3f9e3c075e608e0518de3ecadbff36
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-12 18:39:25 -0400

    Modified Socket to use event listeners when setting up.

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,19 +1,26 @@
 var Socket = function() {
   return new Promise(function(resolve, reject) {
+
     try {
       var url = window.location;
       var socket = new WebSocket("ws://"+url.host);
 
-      socket.onopen = function() {
+      // On an open event, resolve with the socket
+      function handleOpen() {
+        socket.removeEventListener("open", handleOpen);
         resolve(socket);
       };
+      socket.addEventListener("open", handleOpen);
 
-      socket.onerror = function(e) {
+      // On an error event, reject the promise
+      function handleError(e) {
         if(e.message === undefined) {
           e.message = "Connection failed.";
         }
+        socket.removeEventListener("error", handleError);
         reject(e);
       };
+      socket.addEventListener("error", handleError);
 
     } catch(e) {
       reject(e);
