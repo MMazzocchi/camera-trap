eb839e0d58a7d15d679759f4ee5222221c39a858
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-24 20:18:28 -0400

    Equlized histogram before sending to bg subtraction.

--- server/MOG2Comparer.js
+++ server/MOG2Comparer.js
@@ -5,15 +5,17 @@ var MOG2Comparer = function(threshold) {
   var that = new ImageComparer();
 
   // Fields
-  var subtractor = new cv.BackgroundSubtractorMOG2();
+  var subtractor = new cv.BackgroundSubtractorMOG2(100, 16, true);
 
   that.handle = function(image_data) {
     that.log("Processing an image.");
 
     var buffer = Buffer.from(image_data, "base64");
     var mat = cv.imdecode(buffer, cv.IMREAD_COLOR);
+    var gscale = mat.cvtColor(cv.COLOR_BGR2GRAY);
+    var equalized = gscale.equalizeHist();
 
-    var changed = subtractor.apply(mat);
+    var changed = subtractor.apply(equalized);
 
     var mean = changed.mean();
     var value = mean.w;
