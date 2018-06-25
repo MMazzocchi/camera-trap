0b92d08127384ce718e431dc7dd9a1174525fc38
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-25 19:15:58 -0400

    Removed unneeded configuration values.

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const config = {
   "host": "0.0.0.0",
   "port": 9221,
 
-  "comparer_type": "mog2",
-
-  // Histogram comparer configuration
-  "threshold0": 0.998,
-  "threshold1": 50000,
-  "threshold2": 2,
-  "threshold3": 0.15,
-
-  // MOG2 Thresholds
   "mog2_threshold": 35
 };
 
