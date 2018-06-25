cb0643afc412a2af88057acd72c5c7067aee84e4
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-25 18:50:28 -0400

    Removed MOG2 dependency on ImageComparer.

--- server/MOG2Comparer.js
+++ server/MOG2Comparer.js
@@ -1,14 +1,22 @@
-var ImageComparer = require("./ImageComparer.js");
 var cv = require("opencv4nodejs");
 
+var start_id = 0;
+function newId() {
+  var new_id = start_id;
+  start_id += 1;
+  return new_id;
+};
+
 var MOG2Comparer = function(threshold) {
-  var that = new ImageComparer();
+  var that = {};
 
   // Fields
+  var id = newId();
+  var debug = require("debug")("camera-trap:comparer:"+id);
   var subtractor = new cv.BackgroundSubtractorMOG2(100, 16, true);
 
   that.handle = function(image_data) {
-    that.log("Processing an image.");
+    debug("Processing an image.");
 
     var buffer = Buffer.from(image_data, "base64");
     var mat = cv.imdecode(buffer, cv.IMREAD_COLOR);
@@ -19,7 +27,7 @@ var MOG2Comparer = function(threshold) {
 
     var mean = changed.mean();
     var value = mean.w;
-    that.log(value);
+    debug(value);
 
     return {
       "different": (value > threshold),
