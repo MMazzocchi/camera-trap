4a7f223d948859e26843a21ced7befd55f6b108e
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-28 18:06:55 -0400

    Renamed main.js camera.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 
 function build() {
-  return browserify("./client/src/main.js")
+  return browserify("./client/src/camera.js")
     .bundle()
-    .pipe(source("main.js"))
+    .pipe(source("camera.js"))
     .pipe(gulp.dest("./client/dist/"));
 };
 
